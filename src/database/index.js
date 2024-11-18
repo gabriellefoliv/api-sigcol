b543ad589a31b4aac72e802ff326a6bf13bd64b1
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-11-18 12:44:15 -0300

    feat: docker

--- src/database/index.js
+++ src/database/index.js
@@ -7,7 +7,8 @@ const db = mySQL.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_HOST_USER,
     password: process.env.MYSQL_HOST_PASSWORD,
-    database: process.env.MYSQL_HOST_DATABASE
+    database: process.env.MYSQL_HOST_DATABASE,
+    port: process.env.MYSQL_HOST_PORT,
 })
 
 db.getConnection((err) => {
